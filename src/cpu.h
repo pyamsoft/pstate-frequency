83e2e377d1356df0ddf051ab575d33804c3441d6
pyamsoft <pyam.soft@gmail.com>
2014-09-20 11:41:51 -0700

    Added minimum frequency control functions

--- src/cpu.h
+++ src/cpu.h
@@ -25,15 +25,15 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define FILE_CPU_MAX            "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"
-#define FILE_CPU_MIN            "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"
+#define FILE_CPUINFO_MAX_FREQ   "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"
+#define FILE_CPUINFO_MIN_FREQ   "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"
 #define FILE_PSTATE_TURBO       "/sys/devices/system/cpu/intel_pstate/no_turbo"
-#define FILE_PSTATE_MAX_PERCENT "/sys/devices/system/cpu/intel_pstate/max_perf_pct"
-#define FILE_PSTATE_MIN_PERCENT "/sys/devices/system/cpu/intel_pstate/min_perf_pct"
-#define FILE_CPU_FREQ           "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
+#define FILE_CPU_MAX_FREQ       "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
+#define FILE_CPU_MIN_FREQ       "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
 
 struct pyam_cpu_t {
-    char** CPU_FREQ_FILES;
+    char** CPU_MAX_FREQ_FILES;
+    char** CPU_MIN_FREQ_FILES;
 };
 
 struct pyam_cpu_t
@@ -41,30 +41,53 @@ pyam_cpu_create(void);
 
 void
 pyam_cpu_set_turbo(
+        struct pyam_cpu_t* const cpu,
         const int32_t turbo);
 
 int32_t
-pyam_cpu_get_number(void);
+pyam_cpu_get_number(
+        struct pyam_cpu_t* const cpu);
 
 int32_t
-pyam_cpu_get_mhz(void);
+pyam_cpu_get_mhz(
+        struct pyam_cpu_t* const cpu);
 
 void
 pyam_cpu_set_max(
         struct pyam_cpu_t* const cpu,
         const int32_t max);
 
-int32_t
-pyam_cpu_get_min(void);
+void
+pyam_cpu_set_min(
+        struct pyam_cpu_t* const cpu,
+        const int32_t min);
 
 int32_t
-pyam_cpu_get_turbo(void);
+pyam_cpu_get_min(
+        struct pyam_cpu_t* const cpu);
 
 int32_t
-pyam_cpu_get_freq(void);
+pyam_cpu_get_turbo(
+        struct pyam_cpu_t* const cpu);
+
+double
+pyam_cpu_get_max_freq(
+        struct pyam_cpu_t* const cpu);
+
+double
+pyam_cpu_get_min_freq(
+        struct pyam_cpu_t* const cpu);
 
 int32_t
-pyam_cpu_get_max(void);
+pyam_cpu_get_max(
+        struct pyam_cpu_t* const cpu);
+
+double
+pyam_cpu_get_cpuinfo_max(void);
+
+double
+pyam_cpu_get_cpuinfo_min(
+        struct pyam_cpu_t* const cpu);
 
 void
 pyam_cpu_destroy(
