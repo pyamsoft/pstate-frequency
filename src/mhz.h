0f233c1a51a259384d58afdb2ee7bf54dd09ba34
pyamsoft <pyam.soft@gmail.com>
2014-11-26 19:44:46 -0800

    Removed cruft

--- src/mhz.h
+++
@@ -1,8 +0,0 @@
-/* #ifndef C_PYAM_MHZ_H_ */
-/* #define C_PYAM_MHZ_H_ */
-
-/* double estimate_MHz(void); */
-/* double estimate_KHz(void); */
-/* unsigned long long int rdtsc(void); */
-
-/* #endif */
