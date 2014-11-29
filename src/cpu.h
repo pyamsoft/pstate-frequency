37ece6fb628d6c7a50a8e6614bad82ada45ff2d7
pyamsoft <pyam.soft@gmail.com>
2014-11-28 17:55:30 -0800

    Fixed includes, updated copyright

--- src/cpu.h
+++ src/cpu.h
@@ -1,22 +1,22 @@
 /*
-    pstate_frequency Easier control of the Intel p-state driver
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-    For questions please contact P.Yam Software at pyam.soft@gmail.com
-
+*   pstate_frequency Easier control of the Intel p-state driver
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 2 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License along
+*   with this program; if not, write to the Free Software Foundation, Inc.,
+*   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*   For questions please contact P.Yam Software at pyam.soft@gmail.com
+*
 */
 
 #ifndef C_PYAM_CPU_H_
@@ -25,11 +25,11 @@
 #include <stdint.h>
 
 #define DIR_PSTATE              "/sys/devices/system/cpu/intel_pstate"
-#define FILE_SCALING_DRIVER "/sys/devices/system/cpu/cpu0/cpufreq/scaling_driver"
-#define FILE_INFO_MAX_FREQ   "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"
-#define FILE_INFO_MIN_FREQ   "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"
-#define FILE_SCALING_MAX_FREQ       "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
-#define FILE_SCALING_MIN_FREQ       "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
+#define FILE_SCALING_DRIVER     "/sys/devices/system/cpu/cpu0/cpufreq/scaling_driver"
+#define FILE_INFO_MAX_FREQ      "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"
+#define FILE_INFO_MIN_FREQ      "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"
+#define FILE_SCALING_MAX_FREQ   "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
+#define FILE_SCALING_MIN_FREQ   "/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq"
 #define FILE_PSTATE_TURBO       "/sys/devices/system/cpu/intel_pstate/no_turbo"
 #define FILE_PSTATE_MAX         "/sys/devices/system/cpu/intel_pstate/max_perf_pct"
 #define FILE_PSTATE_MIN         "/sys/devices/system/cpu/intel_pstate/min_perf_pct"
@@ -37,9 +37,9 @@
 #define FILE_SCALING_MAX_NAME   "/sys/devices/system/cpu/cpu%d/cpufreq/scaling_max_freq"
 
 struct cpu_t {
-    char** CPU_MAX_FREQ_FILES;
-    char** CPU_MIN_FREQ_FILES;
-    int32_t CPU_NUMBER;
+        char** CPU_MAX_FREQ_FILES;
+        char** CPU_MIN_FREQ_FILES;
+        int32_t CPU_NUMBER;
 };
 
 /**
@@ -59,4 +59,4 @@ struct cpu_t cpu_create(void);
 */
 void cpu_destroy(struct cpu_t *cpu);
 
-#endif  // C_PYAM_CPU_H_
+#endif  /* C_PYAM_CPU_H_ */
--- src/main.c
+++ src/main.c
@@ -1,15 +1,37 @@
+/*
+*   pstate_frequency Easier control of the Intel p-state driver
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 2 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License along
+*   with this program; if not, write to the Free Software Foundation, Inc.,
+*   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*   For questions please contact P.Yam Software at pyam.soft@gmail.com
+*
+*/
+
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+
 #include "src/cpu.h"
 #include "src/cpuread.h"
 #include "src/cpuwrite.h"
 #include "src/color.h"
 #include "src/plan.h"
 #include "src/util.h"
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <getopt.h>
 
 static int32_t handle_result(struct cpu_t *const cpu, const int32_t result,
         int32_t *const flag_action, int32_t *const value_min,
