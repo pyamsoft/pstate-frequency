50414c8b5000d3b6e51365749c4195f49236b589
pyamsoft <pyam.soft@gmail.com>
2015-06-05 16:02:04 -0700

    Refactor the gigantic setCpuValues function in main

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -35,6 +35,174 @@
 
 static bool setCpuValues(const psfreq::Cpu &cpu,
 		const psfreq::Values &cpuValues);
+static void setSaneValues(const psfreq::Cpu &cpu,
+		const int cpuInfoMin,
+		const int cpuInfoMax);
+static bool isSystemInsane(const int cpuInfoMin, const int cpuInfoMax,
+		const int cpuMinPstate, const int cpuMaxPstate,
+		const std::string &cpuGovernor);
+static int sanitizeVal(const int requested, const int cpuPstate,
+		const int cpuInfoMin, const int cpuInfoMax);
+static void setCpuTurbo(const psfreq::Cpu &cpu,
+		const psfreq::Values &cpuValues);
+static void setCpuGovernor(const psfreq::Cpu &cpu,
+		const psfreq::Values &cpuValues,
+		const std::string cpuGovernor);
+static void setCpuFrequencies(const psfreq::Cpu &cpu, const int newMax,
+		const int newMin, const int cpuMinPstate);
+static void sleepCpu(const psfreq::Values &cpuValues);
+
+static void sleepCpu(const psfreq::Values &cpuValues)
+{
+	if (cpuValues.shouldSleep()) {
+		if (psfreq::Log::isDebug()) {
+			std::cout << "[Debug] Sleep for two seconds"
+				<< std::endl;
+		}
+		sleep(2);
+	}
+}
+
+static void setCpuFrequencies(const psfreq::Cpu &cpu, const int newMax,
+		const int newMin, const int cpuMinPstate)
+{
+	/*
+	 * If the new maximum frequency that is requested
+	 * is less than the current minimum, we must
+	 * modify the minimum first before we can actually
+	 * change the max frequency
+	 */
+	if (cpuMinPstate > newMax) {
+		if (psfreq::Log::isDebug()) {
+			std::cout << "[Debug] Current min is higher "
+				<< "than the new max, set the min "
+				<< "first before adjusting max"
+				<< std::endl;
+		}
+		cpu.setScalingMin(newMin);
+		cpu.setScalingMax(newMax);
+	} else {
+		if (psfreq::Log::isDebug()) {
+			std::cout << "[Debug] Current min is lower "
+				<< "than the new max, can safely "
+				<< "adjust the new max"
+				<< std::endl;
+		}
+		cpu.setScalingMax(newMax);
+		cpu.setScalingMin(newMin);
+	}
+}
+
+
+static void setCpuGovernor(const psfreq::Cpu &cpu,
+		const psfreq::Values &cpuValues,
+		const std::string cpuGovernor)
+{
+	/*
+	 * Set the software CPU governor
+	 */
+	if (psfreq::Log::isDebug()) {
+		std::cout << "[Debug] Set the cpu governor"
+				<< std::endl;
+	}
+	const std::string requestedGovernor = cpuValues.getGovernor();
+	const std::string newGovernor =
+			(requestedGovernor != psfreq::UNINITIALIZED_STR
+			? requestedGovernor
+			: cpuGovernor);
+	cpu.setGovernor(newGovernor);
+}
+
+static void setCpuTurbo(const psfreq::Cpu &cpu,
+		const psfreq::Values &cpuValues)
+{
+	/*
+	 * If the system supports a Turbo Boost
+	 * type functionality, attempt to set it
+	 * as well.
+	 */
+	const int cpuTurbo = cpu.getTurboBoost();
+	if (cpuTurbo != psfreq::Cpu::TURBO_BOOST_INSANE) {
+		if (psfreq::Log::isDebug()) {
+			std::cout << "[Debug] Turbo is available"
+				<< std::endl;
+		}
+		const int turbo = cpuValues.getTurbo();
+		int newTurbo = (turbo != -1 ? turbo : cpuTurbo);
+		newTurbo = psfreq::boundValue(newTurbo, 0, 1);
+		cpu.setTurboBoost(newTurbo);
+	}
+}
+
+static int sanitizeVal(const int requested, const int cpuPstate,
+		const int cpuInfoMin, const int cpuInfoMax)
+{
+	/*
+	 * Sanitize the CPU frequency so that
+	 * it can safely be set.
+	 */
+	int newVal = (requested >= 0
+			? requested
+			: cpuPstate);
+	newVal = psfreq::boundValue(newVal, cpuInfoMin, cpuInfoMax);
+	return newVal;
+}
+
+static bool isSystemInsane(const int cpuInfoMin, const int cpuInfoMax,
+		const int cpuMinPstate, const int cpuMaxPstate,
+		const std::string &cpuGovernor)
+{
+	/*
+	 * Check for sane system, if any of these values are not stable,
+	 * exit immediately, this system is not supported by
+	 * pstate-frequency
+	 */
+	return (cpuInfoMin == psfreq::Cpu::INFO_FREQUENCY_INSANE
+			|| cpuInfoMax == psfreq::Cpu::INFO_FREQUENCY_INSANE
+			|| cpuMinPstate == psfreq::Cpu::PSTATE_VALUE_INSANE
+			|| cpuMaxPstate == psfreq::Cpu::PSTATE_VALUE_INSANE
+			|| cpuGovernor == psfreq::Cpu::GOVERNOR_INSANE)
+		? false
+		: true;
+
+}
+
+static void setSaneValues(const psfreq::Cpu &cpu,
+		const int cpuInfoMin,
+		const int cpuInfoMax)
+{
+	/* If intel_pstate only source the max_perf_pct and
+		* min_perf_pct files when they change, then we need to force
+		* a change somehow. Though ugly, setting the CPU first to a
+		* powersave state and then a performance state should force
+		* the driver to re-read the CPU in almost all situations
+		*/
+	if (psfreq::Log::isDebug()) {
+		std::cout << "[Debug] Setting sane min/max values"
+			<< std::endl;
+	}
+	int saneMin = 0;
+	int saneMax = 100;
+	int saneTurbo = cpu.getTurboBoost() == psfreq::Values::PSTATE_TURBO
+		? psfreq::Values::PSTATE_NO_TURBO
+		: psfreq::Values::PSTATE_TURBO;
+	saneMin = psfreq::boundValue(saneMin, cpuInfoMin,
+			cpuInfoMax - 1);
+	saneMax = psfreq::boundValue(saneMax, cpuInfoMin + 1,
+			cpuInfoMax);
+	saneMin = (saneMin >= saneMax
+			? saneMax - 1
+			: saneMin);
+	if (psfreq::Log::isDebug()) {
+		std::cout << "[Debug] Sane Max: " << saneMax
+				<< " Sane Min: " << saneMin
+				<< " Sane Turbo: " << saneTurbo
+				<< std::endl;
+	}
+	cpu.setScalingMin(saneMin);
+	cpu.setScalingMax(saneMax);
+	cpu.setTurboBoost(saneTurbo);
+}
 
 /*
  * Retrieves the values requested by the user and makes sure that they are
@@ -54,16 +222,8 @@ static bool setCpuValues(const psfreq::Cpu &cpu,
 	const int cpuMaxPstate = cpu.getMaxValue();
 	const std::string cpuGovernor = cpu.getGovernor();
 
-	/*
-	 * Check for sane system, if any of these values are not stable,
-	 * exit immediately, this system is not supported by
-	 * pstate-frequency
-	 */
-	if (cpuInfoMin == psfreq::Cpu::INFO_FREQUENCY_INSANE
-			|| cpuInfoMax == psfreq::Cpu::INFO_FREQUENCY_INSANE
-			|| cpuMinPstate == psfreq::Cpu::PSTATE_VALUE_INSANE
-			|| cpuMaxPstate == psfreq::Cpu::PSTATE_VALUE_INSANE
-			|| cpuGovernor == psfreq::Cpu::GOVERNOR_INSANE) {
+	if (!isSystemInsane(cpuInfoMin, cpuInfoMax, cpuMinPstate,
+				cpuMaxPstate, cpuGovernor)) {
 		if (!psfreq::Log::isAllQuiet()) {
 			std::cerr << psfreq::Color::boldRed()
 				  << "[Error] System is insane"
@@ -71,34 +231,16 @@ static bool setCpuValues(const psfreq::Cpu &cpu,
 		}
 		return false;
 	} else {
-		/*
-		 * Sanitize the minimum CPU frequency so that
-		 * it can safely be set.
-		 */
 		if (psfreq::Log::isDebug()) {
 			std::cout << "[Debug] bound the CPU min" << std::endl;
 		}
-		const int requestedMin = cpuValues.getMin();
-		int newMin = (requestedMin >= 0
-				? requestedMin
-				: cpuMinPstate);
-		newMin = psfreq::boundValue(newMin, cpuInfoMin,
-				cpuInfoMax - 1);
-
-		/*
-		 * Sanitize the maximum CPU frequency, including
-		 * the condition that it be greater than the
-		 * minimum, so that it can safely be set.
-		 */
+		int newMin = sanitizeVal(cpuValues.getMin(),
+				cpuMinPstate, cpuInfoMin, cpuInfoMax - 1);
 		if (psfreq::Log::isDebug()) {
 			std::cout << "[Debug] bound the CPU max" << std::endl;
 		}
-		const int requestedMax = cpuValues.getMax();
-		int newMax = (requestedMax >= 0
-				? requestedMax
-				: cpuMaxPstate);
-		newMax = psfreq::boundValue(newMax, cpuInfoMin + 1,
-				cpuInfoMax);
+		int newMax = sanitizeVal(cpuValues.getMax(),
+				cpuMaxPstate, cpuInfoMin + 1, cpuInfoMax);
 		/*
 		 * Make sure that the minimum frequency is not going
 		 * to be greater than or equal to the maximum frequency
@@ -107,95 +249,11 @@ static bool setCpuValues(const psfreq::Cpu &cpu,
 				? newMax - 1
 				: newMin);
 
-		/* If intel_pstate only source the max_perf_pct and
-		 * min_perf_pct files when they change, then we need to force
-		 * a change somehow. Though ugly, setting the CPU first to a
-		 * powersave state and then a performance state should force
-		 * the driver to re-read the CPU in almost all situations
-		 */
-		if (psfreq::Log::isDebug()) {
-			std::cout << "[Debug] Setting sane min/max values"
-				<< std::endl;
-		}
-		int saneMin = 0;
-		int saneMax = 100;
-		saneMin = psfreq::boundValue(saneMin, cpuInfoMin,
-				cpuInfoMax - 1);
-		saneMax = psfreq::boundValue(saneMax, cpuInfoMin + 1,
-				cpuInfoMax);
-		saneMin = (saneMin >= saneMax
-				? saneMax - 1
-				: saneMin);
-		if (psfreq::Log::isDebug()) {
-			std::cout << "[Debug] Sane Max: " << saneMax
-				  << " Sane Min: " << saneMin
-				  << std::endl;
-		}
-		cpu.setScalingMin(saneMin);
-		cpu.setScalingMax(saneMax);
-		if (psfreq::Log::isDebug()) {
-			std::cout << "[Debug] Sleep for two seconds"
-				  << std::endl;
-		}
-		if (cpuValues.shouldSleep()) {
-			sleep(2);
-		}
-
-		/*
-		 * If the new maximum frequency that is requested
-		 * is less than the current minimum, we must
-		 * modify the minimum first before we can actually
-		 * change the max frequency
-		 */
-		if (cpuMinPstate > newMax) {
-			if (psfreq::Log::isDebug()) {
-				std::cout << "[Debug] Current min is higher "
-					<< "than the new max, set the min "
-					<< "first before adjusting max"
-					<< std::endl;
-			}
-			cpu.setScalingMin(newMin);
-			cpu.setScalingMax(newMax);
-		} else {
-			if (psfreq::Log::isDebug()) {
-				std::cout << "[Debug] Current min is lower "
-					<< "than the new max, can safely "
-					<< "adjust the new max"
-					<< std::endl;
-			}
-			cpu.setScalingMax(newMax);
-			cpu.setScalingMin(newMin);
-		}
-		/*
-		 * If the system supports a Turbo Boost
-		 * type functionality, attempt to set it
-		 * as well.
-		 */
-		const int cpuTurbo = cpu.getTurboBoost();
-		if (cpuTurbo != psfreq::Cpu::TURBO_BOOST_INSANE) {
-			if (psfreq::Log::isDebug()) {
-				std::cout << "[Debug] Turbo is available"
-					<< std::endl;
-			}
-			const int turbo = cpuValues.getTurbo();
-			int newTurbo = (turbo != -1 ? turbo : cpuTurbo);
-			newTurbo = psfreq::boundValue(newTurbo, 0, 1);
-			cpu.setTurboBoost(newTurbo);
-		}
-
-		/*
-		 * Set the software CPU governor
-		 */
-		if (psfreq::Log::isDebug()) {
-			std::cout << "[Debug] Set the cpu governor"
-				  << std::endl;
-		}
-		const std::string requestedGovernor = cpuValues.getGovernor();
-		const std::string newGovernor =
-				(requestedGovernor != psfreq::UNINITIALIZED_STR
-				? requestedGovernor
-				: cpuGovernor);
-		cpu.setGovernor(newGovernor);
+		setSaneValues(cpu, cpuInfoMin, cpuInfoMax);
+		sleepCpu(cpuValues);
+		setCpuFrequencies(cpu, newMax, newMin, cpuMinPstate);
+		setCpuTurbo(cpu, cpuValues);
+		setCpuGovernor(cpu, cpuValues, cpuGovernor);
 		return true;
 	}
 }
