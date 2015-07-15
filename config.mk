61ed9ea39ee87c095af4ac2fac1ca64afa77ffb1
pyamsoft <pyam.soft@gmail.com>
2015-07-15 00:52:57 -0700

    The current directory is implicitly included

--- config.mk
+++ config.mk
@@ -88,7 +88,7 @@ LDFLAGS:= -Wl,-O2,--sort-common,--as-needed,-z,relro,-z,now,--strip-all \
 ##
 # Compiler flags
 ##
-CXXFLAGS:= $(STD) -I. -O2 \
+CXXFLAGS:= $(STD) -O2 \
 	-march=native -pipe \
 	-Wall -Wextra -Werror -Wpedantic -Wmissing-declarations \
 	-Wunreachable-code $(CXXFLAGS)
