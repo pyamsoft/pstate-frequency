73e9ddb7f918c85d48b2af5a468d3c1d5b1d0271
pyamsoft <pyam.soft@gmail.com>
2015-10-06 14:44:00 -0700

    [src/main.c] Added interface to set new cpu values
    Added interface which either takes a plan, or raw values, and can update
    the current CPU values accordingly.

--- app/src/main.c
+++ app/src/main.c
@@ -5,10 +5,97 @@
 #include "psfreq_log.h"
 #include "psfreq_option.h"
 #include "psfreq_output.h"
+#include "psfreq_plan.h"
 #include "psfreq_sysfs.h"
 
 unsigned char init_cpu_and_sysfs(psfreq_cpu_type *cpu,
                 psfreq_sysfs_type *sysfs);
+int get_cpu_max(const psfreq_cpu_type *cpu, const psfreq_option_type *options);
+int get_cpu_min(const psfreq_cpu_type *cpu, const psfreq_option_type *options);
+char get_cpu_turbo(const psfreq_cpu_type *cpu, const psfreq_option_type *options);
+unsigned char set_cpu_values_raw(const psfreq_cpu_type *cpu,
+                const psfreq_option_type *options, int *const max,
+                int *const min, int *const turbo, char **const gov);
+char *get_cpu_gov(const psfreq_cpu_type *cpu, const psfreq_option_type *options);
+unsigned char set_cpu(const psfreq_cpu_type *cpu, const psfreq_sysfs_type *sysfs,
+                      const int max, const int min,
+                      const int turbo, const char* const gov,
+                      const unsigned char do_sleep);
+
+unsigned char set_cpu_values_raw(const psfreq_cpu_type *cpu,
+                const psfreq_option_type *options, int *const max,
+                int *const min, int *const turbo, char **const gov)
+{
+        *max = get_cpu_max(cpu, options);
+        if (*max < 0) {
+                psfreq_log_error("set_cpu_values_raw",
+                                "Bad Max %d", *max);
+                return 0;
+        }
+        *min = get_cpu_min(cpu, options);
+        if (*min < 0) {
+                psfreq_log_error("set_cpu_values_raw",
+                                "Bad Min %d", *min);
+                return 0;
+        }
+        *turbo = get_cpu_turbo(cpu, options);
+        if (*turbo < 0) {
+                psfreq_log_error("set_cpu_values_raw",
+                                "Bad Turbo %d", *turbo);
+                return 0;
+        }
+        *gov = get_cpu_gov(cpu, options);
+        if (*gov == NULL) {
+                psfreq_log_error("set_cpu_values_raw",
+                                "Bad Gov %s", gov);
+                return 0;
+        }
+        return 1;
+}
+
+int get_cpu_max(const psfreq_cpu_type *cpu, const psfreq_option_type *options)
+{
+        int max;
+        if (options->cpu_max != CPU_UNDEFINED) {
+                max = psfreq_input_max_from_optarg(options->cpu_max);
+        } else {
+                max = psfreq_cpu_get_scaling_max(cpu);
+        }
+        return max;
+}
+
+int get_cpu_min(const psfreq_cpu_type *cpu, const psfreq_option_type *options)
+{
+        int min;
+        if (options->cpu_min != CPU_UNDEFINED) {
+                min = psfreq_input_min_from_optarg(options->cpu_min);
+        } else {
+                min = psfreq_cpu_get_scaling_min(cpu);
+        }
+        return min;
+}
+
+char get_cpu_turbo(const psfreq_cpu_type *cpu, const psfreq_option_type *options)
+{
+        char turbo;
+        if (options->cpu_turbo != TURBO_UNDEFINED) {
+                turbo = psfreq_input_turbo_from_optarg(options->cpu_turbo);
+        } else {
+                turbo = cpu->pst_turbo;
+        }
+        return turbo;
+}
+
+char *get_cpu_gov(const psfreq_cpu_type *cpu, const psfreq_option_type *options)
+{
+        char *gov;
+        if (options->cpu_governor != GOVERNOR_UNDEFINED) {
+                gov = psfreq_input_gov_from_optarg(options->cpu_governor);
+        } else {
+                gov = cpu->scaling_governor;
+        }
+        return gov;
+}
 
 unsigned char init_cpu_and_sysfs(psfreq_cpu_type *cpu,
                 psfreq_sysfs_type *sysfs)
@@ -17,6 +104,45 @@ unsigned char init_cpu_and_sysfs(psfreq_cpu_type *cpu,
         if (!psfreq_cpu_init(cpu, sysfs)) {
                 return 0;
         }
+        return 1;
+}
+
+unsigned char set_cpu(const psfreq_cpu_type *cpu, const psfreq_sysfs_type *sysfs,
+                      const int max, const int min,
+                      const int turbo, const char* const gov,
+                      const unsigned char do_sleep)
+{
+        const int sane_max = 100;
+        const int sane_min = 0;
+        const int sane_turbo = 1;
+        const char *const sane_gov = "powersave";
+        if (cpu == NULL) {
+                psfreq_log_error("set_cpu",
+                                "cpu is NULL");
+                return 0;
+        }
+        if (sysfs == NULL) {
+                psfreq_log_error("set_cpu",
+                                "sysfs is NULL");
+                return 0;
+        }
+
+        /* Set sane cpu values first */
+        psfreq_cpu_set_max(cpu, sysfs, &sane_max);
+        psfreq_cpu_set_min(cpu, sysfs, &sane_min);
+        psfreq_cpu_set_gov(cpu, sysfs, sane_gov);
+        psfreq_cpu_set_turbo(sysfs, &sane_turbo);
+
+        if (do_sleep) {
+                sleep(2);
+        }
+
+        psfreq_log_debug("set_cpu", "Set cpu values: %d, %d, %d, %s",
+                        max, min, turbo, gov);
+        psfreq_cpu_set_max(cpu, sysfs, &max);
+        psfreq_cpu_set_min(cpu, sysfs, &min);
+        psfreq_cpu_set_gov(cpu, sysfs, gov);
+        psfreq_cpu_set_turbo(sysfs, &turbo);
         return 1;
 }
 
@@ -31,17 +157,8 @@ int main(int argc, char **argv)
                 return EXIT_FAILURE;
         }
 
-        psfreq_log("%d", options.action);
-        psfreq_log("%s", options.cpu_turbo);
-        psfreq_log("%s", options.cpu_max);
-        psfreq_log("%s", options.cpu_min);
-        psfreq_log("%s", options.cpu_governor);
-        psfreq_log("%s", options.cpu_plan);
-        psfreq_log("%u", options.cpu_get_type);
-        psfreq_log("%u", options.cpu_sleep);
-        psfreq_log("%u", options.color_enabled);
-
-        if (options.action == ACTION_TYPE_HELP) {
+        if (options.action == ACTION_TYPE_HELP ||
+                        options.action == ACTION_TYPE_UNDEFINED) {
                 /* Print Help */
                 return EXIT_SUCCESS;
         } else if (options.action == ACTION_TYPE_VERSION) {
@@ -50,18 +167,59 @@ int main(int argc, char **argv)
         }
 
         if (options.action == ACTION_TYPE_CPU_SET) {
-                if (geteuid() == 0) {
-                        if (!init_cpu_and_sysfs(&cpu, &sysfs)) {
-                                return EXIT_FAILURE;
-                        }
-                        /* Set cpu */
-
-                        /* After setting the cpu, re-init the changed areas */
-                        psfreq_cpu_reinit(&cpu, &sysfs);
-                } else {
+                int max;
+                int min;
+                int turbo;
+                char *gov = NULL;
+                if (geteuid() != 0) {
                         psfreq_log_error("main", "You must be root.");
                         return EXIT_FAILURE;
                 }
+                if (!init_cpu_and_sysfs(&cpu, &sysfs)) {
+                        return EXIT_FAILURE;
+                }
+                /* Set cpu */
+                if (options.cpu_plan == PLAN_UNDEFINED) {
+                        if (!set_cpu_values_raw(&cpu, &options, &max,
+                                                &min, &turbo, &gov)) {
+                                psfreq_cpu_destroy(&cpu);
+                                return EXIT_FAILURE;
+                        }
+
+                        /* Actual set */
+                        set_cpu(&cpu, &sysfs, max, min, turbo, gov,
+                                        options.cpu_sleep);
+                } else {
+                        /* Pass max,min,turbo,gov and set by plan */
+                        unsigned char r;
+                        const char plan = \
+                                        psfreq_input_plan_from_optarg(
+                                                options.cpu_plan);
+                        if (plan < 0) {
+                                psfreq_cpu_destroy(&cpu);
+                                return EXIT_FAILURE;
+                        }
+                        r = psfreq_plan_set_cpu(&plan, &max, &min,
+                                        &turbo, &gov);
+                        if (!r) {
+                                psfreq_log_error("main",
+                                                "Failed to set plan.");
+                                if (gov != NULL) {
+                                        free(gov);
+                                }
+                                psfreq_cpu_destroy(&cpu);
+                                return EXIT_FAILURE;
+                        }
+
+                        set_cpu(&cpu, &sysfs, max, min, turbo, gov,
+                                        options.cpu_sleep);
+                        /* Actual set */
+                        if (gov != NULL) {
+                                free(gov);
+                        }
+                }
+                /* After setting the cpu, re-init the changed areas */
+                psfreq_cpu_reinit(&cpu, &sysfs);
         } else if (options.action == ACTION_TYPE_CPU_GET) {
                 if (!init_cpu_and_sysfs(&cpu, &sysfs)) {
                         return EXIT_FAILURE;
