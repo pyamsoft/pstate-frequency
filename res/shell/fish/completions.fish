ac1e2978f8e5713bcf09a3856f91ffd30911686c
Danil Doroshin <ddddanil5555@gmail.com>
2021-11-26 22:26:36 +0300

    fish: Separate options into layers

--- res/shell/fish/completions.fish
+++ res/shell/fish/completions.fish
@@ -1,19 +1,26 @@
+# fish completions for pstate-frequency
+
+set -l noopt "not __fish_contains_opt -s G -s S -s c -s r -s p -s g -s m -s n -s t get set loop plan governor max min turbo"
+
 # Disable file completions
 complete -c pstate-frequency -f
 
-complete -c pstate-frequency -s H -l "help" -d "Display help"
-complete -c pstate-frequency -s V -l "version" -d "Display application version"
+complete -c pstate-frequency -n "$noopt" -s H -l "help" -d "Display help"
+complete -c pstate-frequency  -n "$noopt" -s V -l "version" -d "Display application version"
 
 # [get] options
-complete -c pstate-frequency -s G -l "get" -d "Access current CPU values"
-complete -c pstate-frequency -n "__fish_contains_opt -s G get" -s c -l current -d "Display the current user set CPU values"
-complete -c pstate-frequency -n "__fish_contains_opt -s G get" -s r -l real -d "Display the real time CPU frequences"
-complete -c pstate-frequency -n "__fish_contains_opt -s G get" -l loop -r -d "Continually query real time CPU frequencies with a delay of <time> between loops."
+set -l get_layer "__fish_contains_opt -s G get"
+complete -c pstate-frequency -n "$noopt" -s G -l "get" -d "Access current CPU values"
+complete -c pstate-frequency -n "$get_layer" -s c -l current -d "Display the current user set CPU values"
+complete -c pstate-frequency -n "$get_layer" -s r -l real -d "Display the real time CPU frequences"
+complete -c pstate-frequency -n "$get_layer" -l loop -r -d "Continually query real time CPU frequencies with a delay of <time> between loops."
 
 # [set] options
-complete -c pstate-frequency -s S -l "set" -d "Modify current CPU values"
-complete -c pstate-frequency -n "__fish_contains_opt -s S set" -s p -l plan -r -d "Set a predefined power plan <name>"
-complete -c pstate-frequency -n "__fish_contains_opt -s S set" -s g -l governor -r -d "Set the cpufreq governor <name>"
-complete -c pstate-frequency -n "__fish_contains_opt -s S set" -s m -l max -r -d "Modify current CPU max frequency <number|offset>"
-complete -c pstate-frequency -n "__fish_contains_opt -s S set" -s n -l min -r -d "Modify current CPU min frequency <number|offset>"
-complete -c pstate-frequency -n "__fish_contains_opt -s S set" -s t -l turbo -r -d "Modify current CPU turbo boost <number|name>"
+set -l set_layer "__fish_contains_opt -s S set"
+complete -c pstate-frequency -n "$noopt" -s S -l "set" -d "Modify current CPU values"
+complete -c pstate-frequency -n "$set_layer" -s p -l plan -r -d "Set a predefined power plan <name>"
+complete -c pstate-frequency -n "$set_layer" -s g -l governor -r -d "Set the cpufreq governor <name>"
+complete -c pstate-frequency -n "$set_layer" -s m -l max -r -d "Modify current CPU max frequency <number|offset>"
+complete -c pstate-frequency -n "$set_layer" -s n -l min -r -d "Modify current CPU min frequency <number|offset>"
+complete -c pstate-frequency -n "$set_layer" -s t -l turbo -r -d "Modify current CPU turbo boost <number|name>"
+
